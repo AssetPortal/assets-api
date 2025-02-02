7a9aee8e1a20b18eb865c57f764aebc7d023d646
Leandro Palazzolo <leapalazzolo@gmail.com>
2025-02-02 16:31:21 -0300

    fix: improve error msg

--- packages/api/pkg/error/error.go
+++ packages/api/pkg/error/error.go
@@ -14,4 +14,4 @@ var ErrDeletingAsset = errors.New("error deleting asset in database")
 
 var ErrCreatingAssetIDExists = errors.New("id exists")
 var ErrUploadingFile = errors.New("error uploading file")
-var ErrAssetDoesNotBelongToTheUser = errors.New("asset does not belong to the user")
+var ErrAssetDoesNotBelongToTheUser = errors.New("asset does not exist or do not belong to the user")
